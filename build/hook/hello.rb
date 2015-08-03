0b3298c0f06a5c5a60b1a6a0532a3ed33ade9446
kazu69 <kazu69web@gmail.com>
2015-08-03 22:07:59 +0900

    add Connection

--- build/hook/hello.rb
+++ build/hook/hello.rb
@@ -1,3 +1,14 @@
-h = "hello"
-m =  "from h2o_mruby"
-"#{h} #{m}"
+r = H2O::Request.new
+
+ua = r.headers_in["User-Agent"].to_s
+new_ua = r.headers_in["User-Agent"] = "new-#{ua}-h2o_mruby"
+uri = r.uri
+host = r.hostname
+method = r.method
+query = r.query
+
+msg = "User-Agent:#{ua}\nNew User-Agent:#{new_ua}\npath:#{uri}\nhost:#{host}\nmethod:#{method}\nquery:#{query}\nremote_ip:#{H2O::Connection.new.remote_ip}"
+
+r.log_error msg
+
+H2O.return 200, "OK", msg + "\n"
